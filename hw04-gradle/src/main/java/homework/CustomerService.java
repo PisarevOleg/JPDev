dc076a4babdc664023193c563499ee2c92bfea92
PisarevOleg <pisarev@live.ru>
2022-10-23 22:26:54 +0300

    completed version

--- hw04-gradle/src/main/java/homework/CustomerService.java
+++ hw04-gradle/src/main/java/homework/CustomerService.java
@@ -4,7 +4,7 @@ package homework;
 import java.util.*;
 
 public class CustomerService {
-    private final NavigableMap<Customer, String> map = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+    private final NavigableMap<Customer, String> map = new TreeMap<>(Comparator.comparing(Customer::getScores));
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
